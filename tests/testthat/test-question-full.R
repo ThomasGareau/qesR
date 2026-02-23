397aa0cbaed48bb1d10913acdc8aba4857ab1538
ThomasGareau <thomas.gareau.paquette@umontreal.ca>
2026-02-23 08:43:30 -0500

    Remove testthat from Suggests and drop testthat harness

--- tests/testthat/test-question-full.R
+++
@@ -1,8 +0,0 @@
-test_that("get_question full mode can fall back to label when question is truncated", {
-  dat <- data.frame(x = 1:3)
-  attr(dat$x, "qes_question") <- "To make sure we are talking to"
-  attr(dat$x, "label") <- "How old are you?"
-
-  expect_true(identical(get_question(dat, "x", full = TRUE), "How old are you?"))
-  expect_true(identical(get_question(dat, "x", full = FALSE), "To make sure we are talking to"))
-})
